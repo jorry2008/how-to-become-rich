d1dcbc8ba9e6678c474b37d35650d424c310d505
jorry2008 <9805225557@qq.com>
2023-03-30 23:16:59 +0800

    java项目

---
+++ src/test/java/com/diffcoder/rich/MillionaireTest.java
@@ -0,0 +1,34 @@
+package com.diffcoder.rich;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MillionaireTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void howToBecomeRich() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testGetName() {
+    }
+
+    @Test
+    void testHowToBecomeRich() {
+    }
+}
